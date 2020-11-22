21b45225924ab241ea64906925da026c37f67ec5
gyeongwoopark <geewoo94@gmail.com>
2020-11-22 15:59:53 +0900

    feat: end game

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const loginRouter = require('./login');
 const gamesRouter = require('./games');
 const userRouter = require('./user');
+const historiesRouter = require('./histories');
 
 router.use('/login', loginRouter);
 router.use('/games', gamesRouter);
 router.use('/user', userRouter);
+router.use('/histories', historiesRouter);
 
 module.exports = router;
